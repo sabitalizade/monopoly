1ee698d5795fdaba82b570ac5f44843ce10712e8
sabitalizade <sabitalizade@gmail.com>
2021-10-04 16:50:11 +0400

    init

---
+++ src/views/Login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Login = () => {
+    return (
+        <div className="container">
+            <div className="login">
+                <div className="login-with-friend"></div>
+                <div className="login-random"></div>
+            </div>
+        </div>
+    )
+}
+
+export default Login
